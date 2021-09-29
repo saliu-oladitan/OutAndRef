f4f15792e2faa9a88326141a04c8be7ad9d260da
soladitan <sablik2u@yahoo.com>
2021-09-29 14:09:23 +0100

    Add project files.

---
+++ OutAndRef/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OutAndRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 2;
+
+            Console.WriteLine(x);
+
+            //Five(out x);
+            Double(ref x);
+
+            Console.WriteLine(x);
+
+        }
+
+        static void Five(out int a)
+        {
+            a = 5;
+
+            Console.WriteLine(a);
+        }
+
+        static void Double(ref int a)
+        {
+            a = a * 2;
+
+            Console.WriteLine(a);
+        }
+
+    }
+}
+
+
